b7f5be16f8a33e82f4bc7a5c274cb9c8a9d74809
varsha <Varsha Naveen@DESKTOP-NNSNQ5O>
2020-06-20 13:00:31 +0530

    Initial project import

---
+++ java/com/altimetrik/onlinefoodorderingsystem/rest/MenuItemController.java
@@ -0,0 +1,45 @@
+package com.altimetrik.onlinefoodorderingsystem.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.altimetrik.onlinefoodorderingsystem.domain.MenuItem;
+import com.altimetrik.onlinefoodorderingsystem.domain.MenuItemRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/menuItems")
+public class MenuItemController {
+
+    @Autowired
+    private MenuItemRepository mir;
+
+    @GetMapping("/")
+    public List<MenuItem> getMenItems() {
+        return mir.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public MenuItem findMenuById(@PathVariable("id") Long id) {
+        return mir.findOne(id);
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void upload(@RequestBody List<MenuItem> menuItemList) {
+        mir.save(menuItemList);
+    }
+
+    @DeleteMapping("/")
+    public void deleteAll() {
+        mir.deleteAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        mir.delete(id);
+    }
+
+}
